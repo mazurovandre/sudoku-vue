bf0eead8f8b4d00ebd6bfce268d8edf5c46ea095
Andrey <mazurovandre@gmail.com>
2022-05-15 12:00:38 +0300

    App v0.02

---
+++ src/components/GameCell.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="cell">
+
+        <span></span>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GameCell'
+    }
+</script>
+
+<style lang="scss" scoped>
+.cell {
+    width: 50px;
+    height: 50px;
+    border: none;
+    outline: none;
+    border-top: 1px solid gray;
+    border-left: 1px solid gray;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/GameField.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <CellGroup v-for="i in 9" :key="i"/>
+  </div>
+</template>
+
+<script>
+// import
+import CellGroup from './CellGroup.vue'
+export default {
+  name: 'GameField',
+  components: {
+    CellGroup
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 150px);
+  grid-template-rows: repeat(3, 150px);
+  margin: 0 auto;
+  border-bottom: 2px solid black;
+  border-right: 2px solid black;
+}
+</style>
